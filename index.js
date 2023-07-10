05782d5f437fd2430d75fc34e49333f68e802e38
Adan Nicolas Cabanas Garza <adan.cabanas@gmail.com>
2023-07-10 04:45:36 -0500

    defines looping functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, surprise) {
+    const thankYous = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(number) {
+    for (; number >= 0; number--) {
+        console.log(number);
+    }
+}
